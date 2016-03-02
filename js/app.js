785148bf3aa37f9d5a5257d51a5d7a594cef3334
lotusgraham <gwhitley99@gmail.com>
2016-03-02 16:37:13 -0500

    guess, log etc

--- js/app.js
+++ js/app.js
@@ -1,12 +1,82 @@
-var mainNumber;
+var mysteryNumber;
 
 $(document).ready(function(){
 
 	/*--- Start a New Game ---*/
 	function newGame(){
-			mainNumber = Math.floor((Math.random() * 100) + 1);
+			mysteryNumber = Math.floor((Math.random() * 100) + 1);
+			console.log(mysteryNumber);
+			//use preventDefault at some point
 
 	}
+	newGame();
+
+/*----Response for user input---*/
+
+
+// function logGuessList (){
+// 	$('#guesslist').append("<li>" + userGuess + "</li>");
+// };
+var countArray = 1;
+
+function response(){
+	var userGuess = $('#userGuess').val();
+
+	if (isNaN(userGuess)) {
+			alert("That's not a number, please enter a number");
+			return false;
+	}
+	else if (userGuess >= 100 ){
+		alert('please enter a number between 0 and 100');
+		return false;
+	}
+
+	$('#guessList').append('<li>' + userGuess + '</li>');
+
+  // console.log("<li>" + userGuess + "</li>");
+
+ if (mysteryNumber == userGuess){
+		$('#feedback').text('you win');
+			$('#count').text(countArray++);
+	}
+	else if (Math.abs(mysteryNumber - userGuess) < 10 ) {
+		$('#feedback').text('hot');
+			$('#count').text(countArray++);
+	}
+	else if (Math.abs(mysteryNumber - userGuess) < 20 ) {
+		$('#feedback').text('warm');
+			$('#count').text(countArray++);
+	}
+	else if (Math.abs(mysteryNumber - userGuess) < 30 ) {
+		$('#feedback').text('cold');
+			$('#count').text(countArray++);
+	}
+	else if (Math.abs(mysteryNumber - userGuess) < 50 ) {
+		$('#feedback').text('ice cold');
+			$('#count').text(countArray++);
+	}
+
+
+}
+
+
+
+
+	$('#guessButton').click(function(e){
+		e.preventDefault();
+		response();
+
+		// logGuessList();
+
+	});
+
+
+// 	if (1 == mysteryNumber) {
+// 		console.log("keep guessing buddy")
+// 	}
+// else {
+// 	console.log('is this thing working yet?')
+// };
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
