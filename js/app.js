1f03ee0b9ff49e7b2e854a6be6a21489b278b7e6
lotusgraham <gwhitley99@gmail.com>
2016-04-13 16:47:07 -0400

    cleaned up code and refactored hot or cold app

--- js/app.js
+++ js/app.js
@@ -1,98 +1,87 @@
-var mysteryNumber;
+$(document).ready(function () {
 
-$(document).ready(function(){
+    /*--- Display information modal box ---*/
+    $(".what").click(function () {
+        $(".overlay").fadeIn(1000);
 
-	/*--- Start a New Game ---*/
-	function newGame(){
-			mysteryNumber = Math.floor((Math.random() * 100) + 1);
-			$('#guessList').empty();
-			$('#count').text(0);
-			$('#feedback').text('Make your Guess!');
+    });
 
-
-			console.log(mysteryNumber);
-			//use preventDefault at some point
-
-	}
-
-	newGame();
-
-$('.new').click(newGame);
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function () {
+        $(".overlay").fadeOut(1000);
+    });
 
 
-/*----Response for user input---*/
+    $('#guessButton').click(function (event) {
+        event.preventDefault();
+        feedbackFunction();
+    });
 
-var countArray = 1;
-
-function response(){
-	var userGuess = $('#userGuess').val();
-			$('#userGuess').val('');
-
-	if (isNaN(userGuess) || userGuess === '') {
-			alert("That's not a number, please enter a number");
-			return false;
-	}
-	else if (userGuess >= 100 ){
-		alert('please enter a number between 0 and 100');
-		return false;
-	}
-
-	$('#guessList').append('<li>' + userGuess + '</li>');
-
-  // console.log("<li>" + userGuess + "</li>");
-
- if (mysteryNumber == userGuess){
-		$('#feedback').text('you win');
-			$('#count').text(countArray++);
-	}
-	else if (Math.abs(mysteryNumber - userGuess) < 10 ) {
-		$('#feedback').text('hot');
-			$('#count').text(countArray++);
-	}
-	else if (Math.abs(mysteryNumber - userGuess) < 20 ) {
-		$('#feedback').text('warm');
-			$('#count').text(countArray++);
-	}
-	else if (Math.abs(mysteryNumber - userGuess) < 30 ) {
-		$('#feedback').text('cold');
-			$('#count').text(countArray++);
-	}
-	else if (Math.abs(mysteryNumber - userGuess) < 50 ) {
-		$('#feedback').text('ice cold');
-			$('#count').text(countArray++);
-	}
+    var mysteryNumber;
+    var numberOfGuesses = 0;
 
 
-}
+    var newGame = function () {
+
+        $('#feedback').text('Make your Guess!');
+
+        $('#guessList').empty();
+
+        numberOfGuesses = 0;
+
+        $('#count').text(numberOfGuesses);
+
+        mysteryNumber = Math.ceil(Math.random() * 100);
+
+        console.log(mysteryNumber);
+    };
+
+    $('.new').click(newGame);
+
+    newGame();
 
 
+    var feedbackFunction = function () {
+
+        var userGuess = $('#userGuess').val();
+
+        if (isNaN(userGuess) || userGuess === '') {
+            alert('Please enter a number');
+        }
+        else if (userGuess > 100 || userGuess < 0) {
+            alert('Please enter a number between 0 and 100');
+        }
+        else if ((userGuess % 1) !== 0) {
+            alert('Please enter a whole number');
+        }
+        else {
+
+            if (userGuess == mysteryNumber) {
+                $('#feedback').text('You Win');
+            }
+            else if (Math.abs(mysteryNumber - userGuess) < 10) {
+                $('#feedback').text('Muy Caliente');
+            }
+            else if (Math.abs(mysteryNumber - userGuess) < 20) {
+                $('#feedback').text('Hot');
+            }
+            else if (Math.abs(mysteryNumber - userGuess) < 30) {
+                $('#feedback').text('Warm');
+            }
+
+            else if (Math.abs(mysteryNumber - userGuess) < 40) {
+                $('#feedback').text('cold');
+            }
+            else if (Math.abs(mysteryNumber - userGuess) >= 40) {
+                $('#feedback').text('Ice cold');
+            }
 
 
-	$('#guessButton').click(function(e){
-		e.preventDefault();
-		response();
+            $('#guessList').append('<li>' + userGuess + '</li>');
+            $('#userGuess').val('');
+            numberOfGuesses++;
+            $('#count').text(numberOfGuesses);
 
-		// logGuessList();
-
-	});
-
-
-// 	if (1 == mysteryNumber) {
-// 		console.log("keep guessing buddy")
-// 	}
-// else {
-// 	console.log('is this thing working yet?')
-// };
-
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
+        }
+    };
 });
